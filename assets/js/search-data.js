69a5d909bc7d0f26dec153b7c7effef938c37571
ulbrichdennis <146122164+ulbrichdennis@users.noreply.github.com>
2026-03-11 16:20:09 +0000

    Deploying to gh-pages from @ ulbrichdennis/ulbrichdennis.github.io@303fad1b6e4246a5cbc61aca4b87479bc6165b35 🚀

--- assets/js/search-data.js
+++ assets/js/search-data.js
@@ -412,8 +412,8 @@ ninja.data = [{
           description: "",
           section: "Books",handler: () => {
               window.location.href = "/books/the_godfather/";
-            },},{id: "news-looking-for-a-job-i-would-like-to-return-to-research-or-teaching-at-a-university-preferably-full-time-any-offers-or-suggestions-are-highly-appreciated-thank-you",
-          title: 'Looking for a job! I would like to return to research or teaching...',
+            },},{id: "news-i-am-still-looking-for-a-job-and-would-like-to-return-to-research-or-teaching-at-a-university-preferably-full-time-any-offers-or-suggestions-are-highly-appreciated-thank-you",
+          title: 'I am still looking for a job and would like to return to...',
           description: "",
           section: "News",},{id: "projects-dfg-456849348",
           title: 'DFG 456849348',
